c321d2417b3c9e4f24c5242a74c2c5bf4014eace
mehdiasgari <shayanboy3@gmail.com>
2021-11-12 23:42:24 +0330

    Handler methode added

--- app/src/main/java/com/example/splashscreen/SplashActivity.java
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.splashscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        } , 3000);
     }
 }
